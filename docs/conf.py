93d211c0cbc5fbcb8e475dea878f0e35854df927
Simon Mellerin <simon.mellerin@makina-corpus.com>
2020-03-26 13:03:52 +0100

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,6 @@ from sphinx.locale import _
 
 project = u'Makina Corpus Sphinx Theme'
 slug = re.sub(r'\W+', '-', project.lower())
-version = __version__
-release = __version__
 author = u'Dave Snider, Read the Docs, Inc. & contributors'
 copyright = author
 language = 'en'
